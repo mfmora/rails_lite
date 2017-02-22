3e310189a8833300cef92aa2075f2fb05eb10949
Fernanda <fernandamora88@gmail.com>
2017-02-21 17:54:57 -0800

    finish part 1

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -7,9 +7,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params)
     @req = req
     @res = res
+    @params = params
     @already_built_response = false
   end
 
@@ -54,5 +55,7 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
